a9a57e127bc8f8f92d2dcfa0f752f0059e143046
MrAdhit <adhityarimba97@gmail.com>
2023-11-25 20:53:09 +0700

    optimize

--- src/main.rs
+++ src/main.rs
@@ -7,25 +7,22 @@ use mki::Keyboard;
 fn main() {
     println!("If this program doesn't work, try running it as root");
 
-    thread::spawn(move || {
-        let mut d_s = false;
-        let mut b_ps = false;
-        loop {
-            let c_ps = Keyboard::CapsLock.is_pressed();
-            if c_ps {
-                if !b_ps {
-                    d_s = !d_s;
-                }
-            }
+    let mut d_s = false;
+    let mut b_ps = false;
 
-            if !d_s && c_ps && !b_ps {
+    loop {
+        let c_ps = Keyboard::CapsLock.is_pressed();
+        if c_ps && !b_ps {
+            d_s = !d_s;
+
+            if !d_s {
                 fs::write("/sys/class/leds/input7::capslock/brightness", b"0").unwrap();
                 Keyboard::CapsLock.click();
             }
-
-            b_ps = c_ps;
         }
-    });
 
-    loop { thread::sleep(Duration::from_secs(99999)) }
+        b_ps = c_ps;
+
+        thread::sleep(Duration::from_millis(10));
+    }
 }
